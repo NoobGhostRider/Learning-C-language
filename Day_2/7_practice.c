a83ea6c4a4cf1969e3915847621cc1bdc1d633f1
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-16 23:28:11 +0530

    Deleted EXE file and modified 7practice.c

---
+++ Day_2/7_practice.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int math,english,hindi,social,science;
+    char name[1000];
+    float percentage;
+    printf("Enter Student name : ");
+    scanf("%s",&name);
+    printf("Enter the Maths Marks : ");
+    scanf("%d",&math);
+    printf("Enter the English Marks : ");
+    scanf("%d",&english);
+    printf("Enter the Hindi Marks : ");
+    scanf("%d",&hindi);
+    printf("Enter the Social Marks : ");
+    scanf("%d",&social);
+    printf("Enter the Science Marks : ");
+    scanf("%d",&science);
+    percentage=((math+english+hindi+science+social)*100)/500;
+    printf("%s",name);
+    printf(" has got %.2f",percentage);
+    printf(" % \n");
+    return 0;
+}
