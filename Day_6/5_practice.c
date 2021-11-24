c6116309b3519a706ae6f8b6e050e4d7408fd4f8
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-24 20:31:22 +0530

    done 5th question

--- Day_6/5_practice.c
+++ Day_6/5_practice.c
@@ -2,6 +2,19 @@
 
 #include<stdio.h>
 int main(){
-    
+    int number,n=1,k;
+    printf("Enter the No. to find factorial : ");
+    scanf("%d",&number);
+    int j=number;
+    for (int i = 0; i < j; i++)
+    {
+        if (number>0)
+        {
+            k=n*number;
+            n=k;
+        }
+        number-=1;
+    }
+    printf("Factorial of %d is : %d\n",j,n);
     return 0;
 }
