5e02088ecfa73acb787fc87e42319dd978e7ba02
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-24 19:55:34 +0530

    done questions no. 3

--- Day_6/3_practice.c
+++ Day_6/3_practice.c
@@ -2,6 +2,13 @@
 
 #include<stdio.h>
 int main(){
-    
+    int n=1,i=10,sum=0;
+    do
+    {
+        sum+=n;
+        printf("The sum of natural no. %d is : %d\n",n,sum);
+        n++;
+    } while (n<=10);
+    printf("Total sum of 10 natural no. is : %d\n",sum);
     return 0;
 }
