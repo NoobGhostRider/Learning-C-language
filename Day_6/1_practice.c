d014e840e99e40ce389a1e228f7844e854c75f0c
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-24 19:04:48 +0530

    done with 1st question

--- Day_6/1_practice.c
+++ Day_6/1_practice.c
@@ -2,6 +2,15 @@
 
 #include<stdio.h>
 int main(){
-
+    int number,i=10,table,j=0;
+    printf("Enter the No. : ");
+    scanf("%d",&number);
+    for (j; j<10 ; j++)
+    {
+        table=number*i;
+        printf("%d X %d = %d \n",number,i,table);
+        i--;
+    }
+    
     return 0;
 }
