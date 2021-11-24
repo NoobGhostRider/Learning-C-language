1c1184f8650affdbb0473de104862b095132054a
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-24 19:15:22 +0530

    done 2nd question

--- Day_6/2_practice.c
+++ Day_6/2_practice.c
@@ -2,6 +2,12 @@
 
 #include<stdio.h>
 int main(){
-    
+    int n=0 ,i=10,sum=0;
+    while (n<=i)
+    {
+        sum+=n;
+        n++;
+    }
+    printf("Sum of 10 natural number : %d\n",sum);
     return 0;
 }
