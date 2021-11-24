ca866ae62468944c511d8ace9264681c08ae2cc7
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-24 23:55:42 +0530

    done 7th and 9th question

--- Day_6/7_practice.c
+++ Day_6/7_practice.c
@@ -2,6 +2,22 @@
 
 #include<stdio.h>
 int main(){
-    
+    int num,rev=0,r,n;
+    printf("Enter the Number : ");
+    scanf("%d",&num);
+    n=num;
+    while (num>0)
+    {
+        r=num%10;
+        rev=rev*10+r;
+        num/=10;
+    }
+    if (n==rev)
+    {
+        printf("It is palindrome\n");
+    }
+    else{
+        printf("It is Not palindrome\n");
+    }
     return 0;
 }
--- Day_6/9_practice.c
+++ Day_6/9_practice.c
@@ -2,6 +2,15 @@
 
 #include<stdio.h>
 int main(){
-    
+    int num,rev=0,r;
+    printf("Enter the Number : ");
+    scanf("%d",&num);
+    while (num>0)
+    {
+        r=num%10;
+        rev=rev*10+r;
+        num/=10;
+    }
+   printf("reverse of no. is : %d\n",rev);
     return 0;
 }
