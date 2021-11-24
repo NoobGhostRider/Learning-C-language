fff55bf113cbc6825917f1ef2f2ccf7dba36fe24
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-11-24 20:04:09 +0530

    done 4th question

--- Day_6/4_practice.c
+++ Day_6/4_practice.c
@@ -2,6 +2,13 @@
 
 #include<stdio.h>
 int main(){
-    
+    int n=8,i=1,table,sum=0;
+    for (int j= 1; j < 15; j++)
+    {
+        table=n*i;
+        sum+=table;
+        i++;
+    }
+    printf("The Sum of number occuring in table of 8  : %d\n",sum);
     return 0;
 }
