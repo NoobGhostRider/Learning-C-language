e4da47f6aad720cfa909654e86cfc060bd70f4cb
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-12-03 19:01:31 +0530

    done 4 practice

--- Day_7/4_practice.c
+++ Day_7/4_practice.c
@@ -2,6 +2,15 @@
 
 #include<stdio.h>
 int main(){
-
+    int number,rem,multip=1;
+    printf("Enter The Number : ");
+    scanf("%d",&number);
+    while (number>0)
+    {
+        rem=number%10;
+        number/=10;
+        multip*=rem;
+    }
+    printf("The Multiplication of digit of a number : %d\n",multip);
     return 0;
 }
