90d40929c1260f94145288003e97a85313f63b07
Krishna Kant Maurya <krishnakantmaurya1616@gmail.com>
2021-12-03 18:47:24 +0530

    Done 1_practice

--- Day_7/1_practice.c
+++ Day_7/1_practice.c
@@ -1,7 +1,15 @@
 //WAP to count no. Of digits in a number.
-
 #include<stdio.h>
 int main(){
-
+    int count=0,number,digit,rem;
+    printf("Enter The Number : ");
+    scanf("%d",&number);
+    while(number>0)
+    {
+        rem=number%10;
+        number=number/10;
+        count+=1;    
+    }
+    printf("The Total no. of digit is : %d\n",count);
     return 0;
 }
